e5e453f01128edf654b31918a53b80a8cbff4880
Ankur Sethi <contact@ankursethi.in>
2016-08-02 16:36:38 +0530

    Moving things around, renaming things, general cleanup.

---
+++ src/containers/SoundCloudExplorerApp.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class SoundCloudExplorerApp extends Component {
+  render() {
+    return (
+        <h1>hi i am cool app</h1>
+    );
+  }
+}
+
+export default SoundCloudExplorerApp;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+
+import SoundCloudExplorerApp from './containers/SoundCloudExplorerApp';
+
 import './index.css';
 
 ReactDOM.render(
-  <App />,
+  <SoundCloudExplorerApp />,
   document.getElementById('root')
 );
