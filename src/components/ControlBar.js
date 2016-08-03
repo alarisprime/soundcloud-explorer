ca09c0eac29a1f1c12d8a356943dc22f347a56bb
Ankur Sethi <contact@ankursethi.in>
2016-08-03 19:32:44 +0530

    Added some styling. WIP.

--- src/components/ControlBar.js
+++ src/components/ControlBar.js
@@ -1,10 +1,16 @@
 import React from 'react';
 import Radium from 'radium';
+import * as constants from '../constants';
 
-const STYLE = { };
+const STYLE = {
+  backgroundColor: constants.COLORS.BRAND,
+  display: 'flex',
+  flexDirection: 'row',
+  flexWrap: 'nowrap'
+};
 
 const ControlBar = ({ togglePlay, previous, next }) => (
-  <div>
+  <div style={STYLE}>
     <button onClick={previous}>previous</button>
     <button onClick={togglePlay}>play/pause</button>
     <button onClick={next}>next</button>
--- src/components/SoundCloudPlayer.js
+++ src/components/SoundCloudPlayer.js
@@ -4,13 +4,15 @@ import TopBar from './TopBar';
 import TrackList from './TrackList';
 import ControlBar from './ControlBar';
 
-const style = { };
+const STYLE = {
+  display: 'flex',
+  flexDirection: 'column'
+};
 
 const SoundCloudPlayer = ({ genre, loading, tracks, nowPlaying, togglePlay, next, previous }) => (
-  <div>
+  <div style={STYLE}>
     <TopBar genre={genre} />
-    <h1>album art goes here</h1>
-    {nowPlaying === -1 ? null : <img src={tracks[nowPlaying].artwork_url} alt={tracks[nowPlaying].description}/>}
+    {nowPlaying === -1 ? <div>placeholder</div> : <img src={tracks[nowPlaying].artwork_url} alt={tracks[nowPlaying].description}/>}
     <ControlBar togglePlay={() => togglePlay(nowPlaying)} next={next} previous={previous} />
     <TrackList tracks={tracks} nowPlaying={nowPlaying} togglePlay={togglePlay} />
   </div>
